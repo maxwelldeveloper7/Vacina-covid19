5ab797436a35ba59e94ca19f870efedb0d185606
Maxwell de Oliveira Chaves <maxwellchaves1844@gmail.com>
2021-03-04 14:08:35 -0300

    criando tabela da tela principal

--- src/vacinacovid/visao/Principal.java
+++ src/vacinacovid/visao/Principal.java
@@ -5,6 +5,7 @@
  */
 package vacinacovid.visao;
 
+import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.Image;
 import java.awt.Toolkit;
@@ -13,7 +14,13 @@ import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
+import javax.swing.JPanel;
 import javax.swing.JRootPane;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.SwingConstants;
+import javax.swing.table.DefaultTableCellRenderer;
+import javax.swing.table.DefaultTableModel;
 
 /**
  *
@@ -25,15 +32,21 @@ public class Principal extends JFrame{
     private JMenuBar menu;
     private JMenu mnuCadastros, mnuRelatorios;
     private JMenuItem mnuEntrada, mnuSaida, mnuManutencao;
+    private JTable tabela;
+    private JScrollPane barraRolagem;
+    private DefaultTableModel modelo = new DefaultTableModel();
+    private JPanel pnFundo;
+    
     
     
     public Principal() {
         super("Vacinação contra Covid-19 <<Secretaria Municipal de Saúde>>");
         URL url = this.getClass().getResource("/vacinacovid/visao/favicon.png");
         Image iconeTitulo = Toolkit.getDefaultToolkit().getImage(url);
-        setIconImage(iconeTitulo);
-        construindoTela(800, 600);
+        setIconImage(iconeTitulo);        
         construindoBarraDeMenu();
+        construirTabela();
+        construindoTela(1200, 700);
     }
     
     
@@ -46,6 +59,12 @@ public class Principal extends JFrame{
         getRootPane().setWindowDecorationStyle(JRootPane.FRAME);
         setResizable(false);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
+        
+        pnFundo = new JPanel(new BorderLayout());
+        
+        pnFundo.add(BorderLayout.CENTER, barraRolagem);
+        
+        getContentPane().add(pnFundo);
     }
 
     private void construindoBarraDeMenu() {        
@@ -62,5 +81,48 @@ public class Principal extends JFrame{
         mnuCadastros.add(mnuManutencao);
         
     }
+
+    private void construirTabela() {
+        tabela = new JTable(modelo);
+        
+        DefaultTableCellRenderer direita = new DefaultTableCellRenderer();
+        DefaultTableCellRenderer centro = new DefaultTableCellRenderer();
+        DefaultTableCellRenderer esquerda = new DefaultTableCellRenderer();
+        esquerda.setHorizontalAlignment(SwingConstants.LEFT);
+        centro.setHorizontalAlignment(SwingConstants.CENTER);
+        direita.setHorizontalAlignment(SwingConstants.RIGHT);
+        
+        modelo.addColumn("ID");
+        modelo.addColumn("Nome");
+        modelo.addColumn("Nascmento");
+        modelo.addColumn("Idade");
+        modelo.addColumn("Endereço");
+        modelo.addColumn("CPF");
+        modelo.addColumn("Mãe");
+        modelo.addColumn("ACS");
+        modelo.addColumn("ESF");
+        modelo.addColumn("Resp. pelo Preenchimento");
+        modelo.addColumn("Cargo");
+        
+        tabela.getColumnModel().getColumn(0).setPreferredWidth(50);
+        tabela.getColumnModel().getColumn(1).setPreferredWidth(200);
+        tabela.getColumnModel().getColumn(2).setPreferredWidth(90);
+        tabela.getColumnModel().getColumn(2).setPreferredWidth(80);
+        tabela.getColumnModel().getColumn(4).setPreferredWidth(300);
+        tabela.getColumnModel().getColumn(5).setPreferredWidth(110);
+        tabela.getColumnModel().getColumn(6).setPreferredWidth(200);
+        tabela.getColumnModel().getColumn(7).setPreferredWidth(200);
+        tabela.getColumnModel().getColumn(8).setPreferredWidth(200);
+        tabela.getColumnModel().getColumn(9).setPreferredWidth(150);
+        tabela.getColumnModel().getColumn(9).setPreferredWidth(180);
+        tabela.getColumnModel().getColumn(10).setPreferredWidth(100);
+        
+        
+        tabela.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
+        barraRolagem = new JScrollPane(tabela);
+        
+    }
+    
+    
 
 }
